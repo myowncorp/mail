f31334579bc7b7c5bfb61dd769878b158461f77f
Juan <foruseasprogrammer@gmail.com>
2025-01-22 21:57:20 -0500

    increases the size of the view of the email and displays the body. However, deletes the body of the previous email, and doesn't shrink the size back down. Would like to fix that.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -48,14 +48,26 @@ function compose_email() {
 
 function get_mail(mailbox) {
   console.log(`running inside get_mail for ${mailbox}`);
-  
+
   return fetch(`/emails/${mailbox}`)
     .then((response) => response.json())
     .then((emails) => {
-      // Print emails
       return emails;
-      // ... do something else with emails ...
-    });}
+    });
+}
+
+function get_email(emailID){
+/* This function will take an email passed through from the mailbox, fetched from the get_mail command
+    then it will use the ID to fetch to a predetermined API address of that ID to return only that emails info */
+  console.log(`running inside get_email for ${emailID}`);
+  return fetch(`emails/${emailID}`)
+    .then((response) => response.json())
+    .then((email) => {
+      console.log(email)
+      return email;
+    });
+}
+
 
 function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
@@ -69,17 +81,43 @@ function load_mailbox(mailbox) {
   }</h3>`;
 
   get_mail(mailbox).then((emails) => {
-    console.log("emails received:", emails);
     //create a loop to begin looping over the array of emails
-    for(let i = 0; i < emails.length; i++){
+    for (let i = 0; i < emails.length; i++) {
       // create a div element and store it
       divNode = document.createElement("div");
       // add a class to the elements
       divNode.classList.add("email-item");
       // place the email object into the divNode
       divNode.innerHTML = `From: ${emails[i].sender} on ${emails[i].timestamp} Subject: ${emails[i].subject} `;
-      // apend the divNode with the email object in it onto the emails-view
       document.querySelector("#emails-view").appendChild(divNode);
-    };
-  });
+
+
+      /* create the div for the body data outside of the click before the email.body is grabbed so it doesnt recreate it each click */
+      // create a div for the body
+      emailBodyNode = document.createElement("div");
+
+      // add a class to it
+      emailBodyNode.classList.add("email-body");
+
+      // add an event listener to it
+      divNode.addEventListener("click", function () {
+
+
+        if (!emailBodyNode.body) {
+          // increase the height of the div to 150px
+          this.style.height = "150px";
+
+          //append it to divNode
+          this.appendChild(emailBodyNode);
+
+          // get the email information
+          get_email(emails[i].id).then((email) => {
+            emailBodyNode.innerHTML = `Body: ${email.body}`;
+          });
+
+
+        }
+      });
+    }
+  })
 }
